53f4e2c8d3c44b2e5c21a383e7cbee332b67d9b0
Sergey Vorobyov <cerega.vorobjev@yandex.ru>
2023-04-18 15:17:22 +0300

    fix: Исправить ошибки для автотестов №6

--- controllers/users.js
+++ controllers/users.js
@@ -26,10 +26,11 @@ module.exports.getUsersById = (req, res) => {
     res.status(200).send(user);
   })
   .catch((err) => {
+    console.log(err.name);
     if(err.name === 'NotFound') {
-      res.status(400).send({message: "Пользователь с таким id не найден."});
-    } else if (err.name === "CastError" || "ValidatorError") {
       res.status(404).send({message: "Пользователь с таким id не найден."});
+    } else if (err.name === "CastError" || "ValidatorError") {
+      res.status(400).send({message: "Пользователь с таким id не найден."});
     } else {
       res.status(500).send({message: "Ошибка на сервере."});
     }
